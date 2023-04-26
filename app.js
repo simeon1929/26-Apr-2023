8a36dfb2bbf62e13bf7c90ad1f27afa39947433f
HR <hr@batch.lan>
2023-04-26 11:40:34 +0500

    Let Var Const Difference

--- app.js
+++ app.js
@@ -1,2 +1,42 @@
 console.log('working');
 console.log('status');
+
+// function started 
+
+details = {
+    name : 'simeon',
+    age : 30,
+    mail : 'simeon1929@gmail.com'
+}
+
+console.log('your age is ', details.age);
+
+console.log('diffeence between let Var const');
+
+console.log('LET');
+
+let a = 10
+console.log('let A =' , a);
+
+console.log('a value changes from 10 to 20');
+a = 20
+
+console.log('let changes A = ', a);
+
+console.log(' Var example');
+
+var x = 10 
+console.log('Var = ', x);
+console.log('Var value chnages from 10 to 20');
+x = 20
+
+console.log('var changes= ', x );
+
+console.log('const');
+
+const xy = 20 
+console.log('const xy =', xy);
+xy = 30
+console.log('const chnanges', xy);
+
+// 
