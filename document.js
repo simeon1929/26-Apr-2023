0a8afec1a91ecaad9dbc8fc18a5eb87bbc8ea9e2
HR <hr@batch.lan>
2023-04-27 18:40:10 +0500

    template string

--- document.js
+++ document.js
@@ -10,10 +10,25 @@ let text = 'old'
 let nameReuslt = firsrName + ' is ' + newAge + ' '+ text 
 console.log(nameReuslt);
 
-// let crateDiv = document.createElement('<h1>')
-// console.log(crateDiv);
-// let mainText = `<h1>${firstName + newAge + text}</h1>`
+// template string 
 
-// let html = ''
-// html.inner.html(mainText)
+let FatherName = 'Simeon'
+let wifeName = 'Beulah'
+let fistBaby = 'Evania'
+let secondBaby = 'Abram'
+
+let result = `Father name is ${ FatherName} , Mother name ${ wifeName} and first child name ${ fistBaby} and second baby name ${secondBaby} `
+ console.log('Template string : ', result);
+
+ let html = `<table>
+ <tr>
+     <td>Father Name </td><td>${FatherName}</td>
+     <td>Mother Name </td><td> ${wifeName }</td>
+     <td>First Baby name </td><td></td>
+     <td>Second Baby name </td><td></td>
+ </tr>
+</table>
+ `
+
+ console.log(html);
 
