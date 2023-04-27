74f8da6f0bd5922a38cdb14dcfdfee420e10c8b7
HR <hr@batch.lan>
2023-04-27 15:17:07 +0500

    concat

--- document.js
+++ document.js
@@ -7,6 +7,9 @@ let firsrName = 'Simeon'
 let newAge = 20
 let text = 'old'
 
+let nameReuslt = firsrName + ' is ' + newAge + ' '+ text 
+console.log(nameReuslt);
+
 // let crateDiv = document.createElement('<h1>')
 // console.log(crateDiv);
 // let mainText = `<h1>${firstName + newAge + text}</h1>`
