ad5781ad66e9309807fced02554638dacee9dcfc
HR <hr@batch.lan>
2023-04-30 10:03:05 +0500

    for and condtion loos added

--- condtionLoops.js
+++ condtionLoops.js
@@ -8,3 +8,26 @@ if(ax == 1) {
     console.log('False');
 }
 
+console.log('Number Condition');
+
+
+if(10 < 5) {
+    console.log('True 1');
+}else {
+    console.log('False');
+}
+
+
+// 5 steps walked
+
+//let totalStep = 10
+
+for(let step = 0; step < 10 ; step ++ ) {
+    
+    if(step == 5) {
+        console.log('YOUR  STEPS ARE AVAILABE');
+    } else {
+        console.log('Not avalibale');
+    }
+    console.log('Steps ' , step);
+}
