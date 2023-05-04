c611ae9fae234ad8073702e214af0f805f4b172f
HR <hr@batch.lan>
2023-05-04 11:29:50 +0500

    loops working

---
+++ loops.js
@@ -0,0 +1,32 @@
+console.log('Loops');
+
+// For loops
+
+// Number check 
+
+for(let number = 0; number < 10 ; number++) {
+    console.log('Your number - ', number);
+
+    if(number == 5) {
+        console.log('IF Workded');
+
+    }else {
+        console.log('Else part worked');
+    }
+}
+
+// Arrray for loop
+
+let names = ['Simeon', 'Beulah', 'Evania', 'Abram']
+console.log('Your array Name is = ', names);
+for(let x = 0; x < names.length; x++) {
+    console.log('Your array name- ', names[x]);
+}
+
+// While loops
+let i = 5
+while(i  < 10) {
+    console.log('While loop number', i);
+    i++
+}
+
